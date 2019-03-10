292401957049a1e489393d42d4fe0a4abbfff0b0
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2019-03-11 00:42:08 +0530

    Created Menu Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import { Navbar, NavbarBrand } from 'reactstrap'
 import './App.css';
 import { from } from 'rxjs';
 
+import Menu from './components/MenuComponent';
+
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div>
                 <Navbar dark color="primary">
                     <div className = "container">
                         <NavbarBrand href="/">Con Fusion</NavbarBrand>
                     </div>
                 </Navbar>
+                
+                <Menu />
             </div>
         );
     }
